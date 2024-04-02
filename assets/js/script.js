ec3d5e06270654d1b1ec770a97cf18b7b9e1f0f7
collingsandrew <andrewcollings@outlook.com>
2024-04-02 16:53:21 +0100

    Add return statement to 'shufflecards' function, add function 'newgame' to call 'shuffleboard' function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const gameBoard = document.querySelector('.cards-board');
+
 // function to shuffle the cards on the game board
 // the fisher-yates sorting algorithm has been used to shuffle the array of cards https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
 // array destructuring used to shuffle the array for each node
@@ -6,6 +8,7 @@ function shuffleCards(cards) {
         const j = Math.floor(Math.random() * (i + 1));
         [cards[i], cards[j]] = [cards[j], cards[i]];
     }
+    return cards;
 }
 
 // function to use the shuffledCards function and place the shuffled array to its new position on the board
@@ -19,4 +22,9 @@ function shuffleBoard(cardsContainer) {
     shuffledGameCards.forEach(function(card) {
         cardsContainer.appendChild(card);
     });
+}
+
+// function to start a new game and reset/shuffle the board
+function newGame(evt) {
+    shuffleBoard(gameBoard);
 }
