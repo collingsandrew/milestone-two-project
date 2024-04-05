2887f44b610797cd7b1ed03e040b9b2c1748e3da
collingsandrew <andrewcollings@outlook.com>
2024-04-05 17:14:03 +0100

    Add comments to describe the purpose of the javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@ let flippedCard1, flippedCard2;
 let correctMatches = document.querySelector('#correct-matches');
 let matches = 0;
 
-// add event listener for when the HTML is fully loaded, ensuring javascript does not run until then
+// event listener for when the HTML is fully loaded, ensuring javascript does not run until then
 document.addEventListener('DOMContentLoaded', function() {
 });
 
 // function to shuffle the cards on the game board
 // the fisher-yates sorting algorithm has been used to shuffle the array of cards https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
-// array destructuring used to shuffle the array for each node
+// array destructuring used to shuffle each index with the shuffled index
 function shuffleCards(cards) {
     for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -32,25 +32,31 @@ function shuffleBoard(cardsContainer) {
     });
 }
 
-// forEach loop with an event listener that adds the class 'flip-card' to each game card that is clicked, resulting in the card rotating 180 degrees
+// forEach loop with an event listener that flips the card and checks whether the two cards match
 document.querySelectorAll('.game-card').forEach(card => {
     card.addEventListener('click', () => {
+        // if the card is already flipped, exit the function
         if (card.classList.contains('flip-card')) {
             return;
         }
-
+        // add the flip-card class that rotates the card 180 degrees
         card.classList.add('flip-card');
 
+        // if flippedCard1 does not have the value of card then give it the value, this will be the first card flipped
         if (!flippedCard1) {
             flippedCard1 = card;
         } else {
+            // if flippedCard1 does have the value of card, give flippedCard2 the value
             flippedCard2 = card;
+            // check if the two flipped cards have the same inner HTML (img tags and src)
             if (card.innerHTML === flippedCard1.innerHTML) {
+                // if the two cards match, increase the correct matches counter by 1 and reset the flipped cards values
                 matches ++;
                 correctMatches.innerHTML = matches;
                 flippedCard1 = null;
                 flippedCard2 = null;
             }  else {
+                // if the two cards do not match, flip the cards back over after a short period of time
                 setTimeout(() => {
                     card.classList.remove('flip-card');
                     flippedCard1.classList.remove('flip-card');
