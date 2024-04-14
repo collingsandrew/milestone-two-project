7926710ad1c24bead0eb2da0f69a5f1da1f98f9a
collingsandrew <andrewcollings@outlook.com>
2024-04-14 16:04:39 +0100

    Update sendMail function error, remove onclick event, change to eventlistener, add comments

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,19 +1,16 @@
 /*jshint esversion: 6 */ 
 
-function sendMail() {
-    let params = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    };
+// events listener that sends an email using the amialjs api when the user submits the contact form
+// code taken from the emailjs docs https://www.emailjs.com/docs/tutorial/creating-contact-form/
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    const serviceID = 'service_9bd1whl';
-    const templateID = 'template_858quls';
-
-    emailjs.sendForm(serviceID, templateID, params)
-    .then(() => {
-        alert('SUCCESS!');
-    }, (error) => {
-        alert('FAILED...');
+        emailjs.sendForm('service_9bd1whl', 'template_858quls', this)
+            .then(() => {
+                alert('Thank you, your message was sent successfully.');
+            }, (error) => {
+                alert('Oops, looks like something went wrong, please try again', error);
+            });
     });
 }
